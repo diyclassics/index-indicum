8067796f567a49324f02a07c3fed61a644f28a86
Fanny Mezard <fanny@NACOLA01.ISAW.NYU.EDU>
2018-06-04 16:07:57 -0400

    Plain list of authors

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
 import os
 from flask import Flask, render_template
-from pprint import pprint
+import requests
+from lxml import html
 
 app = Flask(__name__)
-#app.config.from_object(os.environ['APP_SETTINGS'])
+# app.config.from_object(os.environ['APP_SETTINGS'])
+
 
 @app.route('/')
 def homepage():
     return render_template('index.html')
 
+
 @app.route('/authors')
 def get_authors():
-    pass
-#    return render_template('authors.html')
+    authors = []
+    for i in range(1, 14):
+        url = 'http://dlib.nyu.edu/awdl/isaw/isaw-papers/'+str(i)+'/'
+        page = requests.get(url)
+        html_content = html.fromstring(page.content)
+        author_elements = html_content.xpath('//meta[@name="DC.creator"]')
+        for author in author_elements:
+            authors.append(author.attrib['content'])
+    return render_template('author.html', authors=authors)
+
 
 if __name__ == '__main__':
     app.run()
-
