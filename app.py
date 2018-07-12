0bc393e1adc2c357eab890bfec035fd61982faf0
Fanny Mezard <fanny@NACOLA01.ISAW.NYU.EDU>
2018-07-12 09:56:39 -0400

    adding some css and a dropdown with links to the different maps

--- app.py
+++ app.py
@@ -225,8 +225,7 @@ def map_places(**kwargs):
 
     if not places :
         flash("We do not have any places associated with that article", "warning")
-        print("blop")
-    return render_template('map.html', places=places, article=article)
+    return render_template('map.html', places=places, article=article, PAPERS_URLS=PAPERS_URLS)
 
 st = StanfordNERTagger('./stanford-ner/classifiers/english.all.3class.distsim.crf.ser.gz',
                        './stanford-ner/stanford-ner.jar',
