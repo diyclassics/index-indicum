7c55086aea8a6b514a0d3e1cdf8b5cf525cf902e
Fanny Mezard <fanny@NACOLA01.ISAW.NYU.EDU>
2018-07-23 10:42:24 -0400

    pleiades info stored in the app #45

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from nameparser import HumanName
 from tqdm import tqdm #for progress bar
 from lxml.etree import tostring
 import math
+import json
 
 # Imports
 
@@ -127,86 +128,27 @@ def get_places():
             places_data[f'ISAW Papers {i}'] = places
     return render_template('places.html', places_data=places_data)
 
-def places_dict(html_contents, i):
-    for html_content in html_contents :
-        places = dict()
-        place_name = html_content.xpath('//a[starts-with(@href,"https://pleiades.stoa.org/place")]/text()')
-        place_pleiades = html_content.xpath('//a[starts-with(@href,"https://pleiades.stoa.org/place")]/@href')
 
-        for j in range(len(place_name)):
-            data = requests.get(place_pleiades[j] + "/json")
-            place_pid = html_content.xpath('//a[starts-with(@href,"%s")]/ancestor::p/@id' % place_pleiades[j])
-            place_captionid = (html_content.xpath('//a[starts-with(@href,"%s")]/ancestor::figure/@id' % place_pleiades[j]))
-            for place in place_captionid :
-                place_pid.append(place)
-            text_pid_list = html_content.xpath('//a[starts-with(@href,"%s")]/ancestor::p' % place_pleiades[j])
-            text_captionid_list = html_content.xpath('//a[starts-with(@href,"%s")]/ancestor::figure' % place_pleiades[j])
-
-            for text in text_captionid_list :
-                text_pid_list.append(text)
-            for k, texte in enumerate(text_pid_list) :
-                text_pid = ""
-                for t in texte :
-                    t = tostring(t, encoding="unicode")
-                    text_pid += t
-                text_pid_list[k] = text_pid.replace('\n', '').replace("'", '"')
-                text_pid_list[k] = (text_pid_list[k]).split(place_name[j])
-                text_pid_list[k][0] = text_pid_list[k][0].split(" ")
-                bracket_before = False
-                bracket_after = False
-                if len(text_pid_list[k][0]) > 100:
-                    bracket_before = True
-
-                if len(text_pid_list[k]) > 1 :
-                    text_pid_list[k][1] = text_pid_list[k][1].split(" ")
-                    if len(text_pid_list[k][1]) > 100:
-                        bracket_after = True
-                    text_pid_list[k] = text_pid_list[k][0][-100:] + ["<b>" + place_name[j] + "</b>"] + text_pid_list[k][1][:100]
-                else :
-                    text_pid_list[k] = text_pid_list[k][0][-100:] + ["<b>" + place_name[j]]
-                text_pid_list[k] = " ".join(text_pid_list[k])
-                incomplete_tags_beginning = re.findall(r"^[^<]*>", text_pid_list[k])
-                incomplete_tags_end = re.findall(r"<[^>]*$", text_pid_list[k])
-                incomplete_tags = incomplete_tags_beginning + incomplete_tags_end
-                for tag in incomplete_tags:
-                    text_pid_list[k] = text_pid_list[k].replace(tag, "")
-                if bracket_after  :
-                    text_pid_list[k] =  text_pid_list[k] + "[...]"
-                if bracket_before  :
-                    text_pid_list[k] = "[...]" + text_pid_list[k]
-
-            url_pid = list()
-            for id in place_pid :
-                id = BASE_URL + str(i) + "/#" + id
-                url_pid.append(id)
-            try :
-                coordinates = data.json()['features'][0]['geometry']['coordinates']
-                coordinates.reverse()
-            except :
-                coordinates = []
-            if coordinates and type(coordinates[0]) is not list :
-                places[place_name[j]] = [place_pleiades[j], str(coordinates), url_pid, [str(i)], text_pid_list]
-
-    return places
 
 @app.route('/map/<article_id>')
 @app.route('/<article_id>/map')
 @app.route('/map')
 def map_places(**kwargs):
     if kwargs :
-        with open("data/papers/isaw-papers-%s.xhtml" % (kwargs["article_id"]), "r") as paper:
-            html_contents = [html.parse(paper)]
-        i = kwargs["article_id"]
 
-        places = places_dict(html_contents, i)
+        i = kwargs["article_id"]
+        places = dict()
+        with open("data/places.json", "r") as places_json:
+            places_article = json.load(places_json)
+        for k,v in places_article.items() :
+            if places_article[k][3] == [str(i)] :
+                places[k] = v
         article = "ISAW Papers " + str(i)
     else :
         article = "ISAW Papers"
         places = dict()
-        for i, url in enumerate(PAPERS_URLS, 1):
-            with open("data/papers/isaw-papers-%s.xhtml" % (i), "r") as paper:
-                html_contents = [html.parse(paper)]
-            places_article = places_dict(html_contents, i)
+        with open ("data/places.json", "r") as places_json :
+            places_article = json.load(places_json)
             for k,v in places_article.items() :
                 if k in places :
                     for p in places_article[k][2] :
@@ -225,7 +167,6 @@ def map_places(**kwargs):
 
     if not places :
         flash("We do not have any places associated with that article", "warning")
-        print("blop")
     return render_template('map.html', places=places, article=article)
 
 st = StanfordNERTagger('./stanford-ner/classifiers/english.all.3class.distsim.crf.ser.gz',
