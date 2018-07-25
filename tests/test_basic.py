f3cde3061b6da38acd1bbe07bbf973a48605283b
Patrick J. Burns <patrick@diyclassics.org>
2018-07-25 14:10:28 -0400

    Create testing shell

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,42 +1,42 @@
-# import os
-# import sys
-# import unittest
+import os
+import sys
+import unittest
 # sys.path.append("..")
-# 
+#
 # import app
-# 
-# class BasicTests(unittest.TestCase):
-# 
-#     ############################
-#     #### setup and teardown ####
-#     ############################
-# 
-#     # executed prior to each test
-#     def setUp(self):
-#         pass
-# 
-#     # executed after each test
-#     def tearDown(self):
-#         pass
-# 
+#
+class BasicTests(unittest.TestCase):
+
+    ############################
+    #### setup and teardown ####
+    ############################
+
+    # executed prior to each test
+    def setUp(self):
+        pass
+
+    # executed after each test
+    def tearDown(self):
+        pass
+
 # ###############
 # #### tests ####
 # ###############
-# 
+#
 #     def test_main_page(self):
 #         # response = self.get('/', follow_redirects=True)
 #         # self.assertEqual(response.status_code, 200)
 #         pass
-#     
-# 
+#
+#
 # class HelperTests(unittest.TestCase):
-#     
+#
 #     def test_sort_names(self):
 #         ### WRITE TEST CODE HERE
 #         self.assertEqual(app._sort_names(["Alexander Jones", "Sebastian Heath"]), ["Sebastian Heath", "Alexander Jones"])
-# 
+#
 #         pass
-# 
-#     
+#
+#
 # if __name__ == "__main__":
 #     unittest.main()
