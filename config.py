1974a7dd5c152c3ef112d8795a6ee0b2ab7af040
Patrick J. Burns <patrick@diyclassics.org>
2018-07-25 14:38:29 -0400

    Update config

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'This key need to be changed and kept secret'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    # SECRET_KEY = 'This key need to be changed and kept secret'
